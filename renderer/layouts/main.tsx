7da0ada1845b576b043bae560634ec90c3765fb4
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2021-03-21 17:10:23 +0800

    add main layout

---
+++ renderer/layouts/main.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Head from 'next/head';
+import { Box, Heading } from '@chakra-ui/layout';
+
+export interface MainLayoutProps {
+  title: string;
+}
+
+const MainLayout: React.FC<MainLayoutProps> = ({ title, children }) => {
+  return (
+    <>
+      <Head>
+        <title>Workspace</title>
+      </Head>
+
+      <Box>
+        <Heading fontWeight="normal" mb={10}>
+          {title}
+        </Heading>
+        {children}
+      </Box>
+    </>
+  );
+};
+
+export default MainLayout;
