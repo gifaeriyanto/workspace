437d3182fe3f281ef5168c5d77f29358389476fc
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2021-03-21 16:18:56 +0800

    add component folder

---
+++ renderer/components/folder/folder.tsx
@@ -0,0 +1,30 @@
+import { Box, Text } from '@chakra-ui/layout';
+import { ComponentWithAs } from '@chakra-ui/system';
+import FolderIcon from 'components/icons/folderIcon';
+import React from 'react';
+
+export interface FolderProps {
+  name: string;
+}
+
+const Folder: ComponentWithAs<'svg', FolderProps> = ({ name, ...props }) => {
+  return (
+    <Box
+      w="150px"
+      h="150px"
+      {...props}
+      textAlign="center"
+      border="16px solid transparent"
+      overflow="hidden"
+      cursor="pointer"
+      borderRadius="lg"
+      transition=".1s background-color"
+      _hover={{ bgColor: 'rgba(0, 0, 0, 0.5)' }}
+    >
+      <FolderIcon w="80px" h="60px" mb={3} mt={1} />
+      <Text fontSize="sm">{name}</Text>
+    </Box>
+  );
+};
+
+export default Folder;
