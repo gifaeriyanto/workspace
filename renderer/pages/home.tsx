ab4579f47c9c9ae98d1fd5f5dda0d062a17fabe2
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2021-03-21 17:10:55 +0800

    UI home page

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,23 +1,42 @@
 import React from 'react';
 import Head from 'next/head';
-import Link from 'next/link';
+import { Box, Flex, Grid, Heading, Text } from '@chakra-ui/layout';
+import Folder from 'components/folder/folder';
+import FolderAddIcon from 'components/icons/folderAddIcon';
+import MainLayout from 'layouts/main';
 
 const Home = () => {
+  const data = new Array(10).fill('');
+
   return (
-    <React.Fragment>
-      <Head>
-        <title>Home - Nextron (with-typescript)</title>
-      </Head>
-      <div>
-        <p>
-          ⚡ Electron + Next.js ⚡ -
-          <Link href="/next">
-            <a>Go to next page</a>
-          </Link>
-        </p>
-        <img src="/images/logo.png" />
-      </div>
-    </React.Fragment>
+    <MainLayout title="Workspace">
+      <Grid
+        gap={2}
+        templateColumns={[
+          'repeat(2, 1fr)',
+          'repeat(4, 1fr)',
+          'repeat(4, 1fr)',
+          'repeat(6, 1fr)',
+          'repeat(8, 1fr)',
+        ]}
+      >
+        <Flex
+          w="150px"
+          h="150px"
+          direction="column"
+          align="center"
+          border="1px solid rgba(255, 255, 255, 0.1)"
+          borderRadius="lg"
+          justify="center"
+        >
+          <FolderAddIcon w="46px" h="35px" />
+          <Text mt={6}>Add project</Text>
+        </Flex>
+        {data.map((_, key) => (
+          <Folder key={key} name="typeracer-flash" />
+        ))}
+      </Grid>
+    </MainLayout>
   );
 };
 
