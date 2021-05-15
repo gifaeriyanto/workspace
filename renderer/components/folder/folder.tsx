04631f63e41c888fee057a83abba1cd682627b3e
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2021-05-16 03:28:07 +0800

    Update folder style

--- renderer/components/folder/folder.tsx
+++ renderer/components/folder/folder.tsx
@@ -1,4 +1,4 @@
-import { Box, Text } from '@chakra-ui/layout';
+import { Box, Flex, Text } from '@chakra-ui/layout';
 import { ComponentWithAs } from '@chakra-ui/system';
 import FolderIcon from 'components/icons/folderIcon';
 import React from 'react';
@@ -9,21 +9,34 @@ export interface FolderProps {
 
 const Folder: ComponentWithAs<'svg', FolderProps> = ({ name, ...props }) => {
   return (
-    <Box
+    <Flex
       w="150px"
       h="150px"
-      {...props}
       textAlign="center"
-      border="16px solid transparent"
       overflow="hidden"
       cursor="pointer"
       borderRadius="lg"
+      alignItems="center"
+      justify="center"
       transition=".1s background-color"
       _hover={{ bgColor: 'rgba(0, 0, 0, 0.5)' }}
+      {...props}
     >
-      <FolderIcon w="80px" h="60px" mb={3} mt={1} />
-      <Text>{name}</Text>
-    </Box>
+      <Box minW={0}>
+        <FolderIcon w="120px" h="90px" borderRadius="lg" p={4} />
+        <Text
+          h="42px"
+          css={{
+            display: '-webkit-box',
+            WebkitLineClamp: 2,
+            WebkitBoxOrient: 'vertical',
+            overflow: 'hidden',
+          }}
+        >
+          {name}
+        </Text>
+      </Box>
+    </Flex>
   );
 };
 
