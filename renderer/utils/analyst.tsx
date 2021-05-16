61564b29c9efb1cdb54243fb579ee83e1d5f7d3b
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2021-05-16 11:15:48 +0800

    Add new utility - analyst

---
+++ renderer/utils/analyst.tsx
@@ -0,0 +1,23 @@
+import { readFileSync } from 'fs';
+import { parse } from 'recast';
+import ts from 'typescript';
+
+export const convertToAst = (dir: string) => {
+  const node = ts.createSourceFile(
+    'analyst.ts',
+    readFileSync(dir, 'utf8'),
+    ts.ScriptTarget.Latest,
+  );
+
+  const imports = {};
+
+  node.forEachChild((child) => {
+    if (ts.SyntaxKind[child.kind] === 'ImportDeclaration') {
+      const ast = parse(child.getText(node)).program.body[0];
+      const source = ast.source.value;
+      imports[source] = 1;
+    }
+  });
+
+  return imports;
+};
