9b5cd0573b6bf03b581e7ea2f5d5a99e40fabe53
Gifa Eriyanto <gifa.eriyanto@gmail.com>
2021-05-16 11:15:16 +0800

    Update fs utility

--- renderer/utils/fs.tsx
+++ renderer/utils/fs.tsx
@@ -1,14 +1,62 @@
 import fs from 'fs';
+import path from 'path';
 
-export const getDirectories = (path: string) => {
-  return fs.readdirSync(path).filter(function (file) {
-    return fs.statSync(path + '/' + file).isDirectory();
+export const getDirectories = (dir: string) => {
+  return fs.readdirSync(dir).filter(function (file) {
+    return fs.statSync(dir + '/' + file).isDirectory();
   });
 };
 
-export const readJSONFile = (path: string, callback: (result: any) => void) => {
-  fs.readFile(path, 'utf8', (err, data) => {
+export const readJSONFile = (dir: string, callback: (result: any) => void) => {
+  fs.readFile(dir, 'utf8', (err, data) => {
     if (err) throw err;
     callback(JSON.parse(data));
   });
+};
+
+export const findFilesFromDir = (
+  dir: string,
+  extensions: string[],
+  callback?: (value: string[]) => void,
+) => {
+  if (!fs.existsSync(dir)) {
+    console.log('no dir ', dir);
+    return;
+  }
+
+  const result = [];
+  const folderExeptions = ['node_modules', 'public', 'dist', 'coverage'];
+  const files = fs.readdirSync(dir, { withFileTypes: true });
+  files
+    .filter((file) => {
+      if (
+        (file.isDirectory() && folderExeptions.includes(file.name)) ||
+        file.name.charAt(0) === '.'
+      ) {
+        return false;
+      }
+
+      if (!file.isDirectory()) {
+        const splittedName = file.name.split('.');
+        const ext = splittedName[splittedName.length - 1];
+        if (extensions.includes(ext)) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+
+      return true;
+    })
+    .forEach((file) => {
+      const filename = path.join(dir, file.name);
+      if (file.isDirectory()) {
+        findFilesFromDir(filename, extensions, callback);
+      } else {
+        result.push(filename);
+        if (callback) {
+          callback(result);
+        }
+      }
+    });
 };
